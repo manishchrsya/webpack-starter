58321940b9d6484f9fe5bc87409a6cf5c884724a
Manish Chaurasiya <chaurasiya.mns@gmail.com>
2024-09-05 10:36:13 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-import { run } from "./app/app";
-import "./main.scss";
 import { AlertService } from "./app/alert.service";
+import "./main.scss";
+import { run } from "./app/app";
 import { ComponentService } from "./app/component.service";
+
 const alertService = new AlertService();
 const componentService = new ComponentService();
+
 run(alertService, componentService);
-console.log("J");
--- webpack.common.js
+++ webpack.common.js
@@ -1,16 +1,25 @@
-const path = require("path");
 var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: {
-    main: "./src/index.js",
-    vendor: "./src/vendor.js"
-  },
+  entry: "./src/index.js",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
   module: {
     rules: [
       {
+        test: /\.scss$/,
+        use: [
+          "style-loader", // 3. inject styles into dom
+          "css-loader", // 2. Turns css into common js
+          "sass-loader", // 1. Turns sass into css
+        ],
+      },
+      {
         test: /\.html$/,
-        use: ["html-loader"]
+        use: ["html-loader"],
       },
       {
         test: /\.(svg|png|jpg|gif)$/,
@@ -18,10 +27,10 @@ module.exports = {
           loader: "file-loader",
           options: {
             name: "[name].[hash].[ext]",
-            outputPath: "imgs"
-          }
-        }
-      }
-    ]
-  }
+            outputPath: "imgs",
+          },
+        },
+      },
+    ],
+  },
 };
